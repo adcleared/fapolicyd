f2e2278a4ddf0f7293358909ef5bb328530675c9
adcleared <202732078+adcleared@users.noreply.github.com>
2026-07-10 20:17:40 +1000

    Async log in debug mode
    (cherry picked from commit ae0f04fde0a260fd0de89eeba493b320554073e0)

--- src/daemon/fapolicyd.c
+++ src/daemon/fapolicyd.c
@@ -1093,6 +1093,7 @@ int main(int argc, const char *argv[])
 	struct sigaction sa;
 	struct rlimit limit;
 	nfds_t pfd_count = 2;
+	int rc;
 
 	setlocale(LC_TIME, "");
 
@@ -1227,17 +1228,17 @@ int main(int argc, const char *argv[])
 		}
 		set_message_mode(MSG_SYSLOG, DBG_NO);
 		openlog("fapolicyd", LOG_PID, LOG_DAEMON);
-		int rc = message_async_start();
-		if (rc) {
-			msg(LOG_ERR,
-			    "Failed starting async logging thread (%s)",
-			    strerror(rc));
-			exit(1);
-		}
-		if (atexit(message_async_stop))
-			msg(LOG_WARNING,
-			    "Cannot register async log exit handler");
 	}
+	rc = message_async_start();
+	if (rc) {
+		msg(LOG_ERR,
+		    "Failed starting async logging thread (%s)",
+		    strerror(rc));
+		exit(1);
+	}
+	if (atexit(message_async_stop))
+		msg(LOG_WARNING,
+		    "Cannot register async log exit handler");
 	state_report_log_reset_strategy(config.reset_strategy);
 	decision_timing_apply_config(config.timing_collection);
 
@@ -1343,7 +1344,6 @@ int main(int argc, const char *argv[])
 
 	msg(LOG_INFO, "Starting to listen for events");
 	while (!stop) {
-		int rc;
 		if (hup) {
 			hup = false;
 			msg(LOG_DEBUG, "Got SIGHUP");
